3db6d216b9c2dd595b3e7c5d6ef6d2530eabf3fb
Simon Cambier <cambier.simon@gmail.com>
2014-11-21 14:01:25 +0100

    Create converter.py

---
+++ converter.py
@@ -0,0 +1,55 @@
+import glob
+import os
+from time import sleep
+import hashlib
+from subprocess import call
+
+
+def main():
+    while True:
+        for filename in glob.glob("*.ase"):
+
+            # Try to find an existing instance for that file
+            file = AseFile.find_file(filename)
+
+            if file:
+                # check if md5 changed between passes
+                newFile = AseFile(filename)
+                if (newFile.hash != file.hash):
+                    newFile.export()
+                    AseFile.files.remove(file)
+                    AseFile.files.append(newFile)
+                    pass
+            else:
+                # Create an instance and save it
+                file = AseFile(filename)
+                file.export()
+                AseFile.files.append(file)
+
+        sleep(2)
+
+
+class AseFile:
+    files = []
+
+    def __init__(self, name):
+        self.name = name
+        self.hash = self.get_hash()
+        self.png_name = os.path.splitext(self.name)[0] + ".png"
+
+    def get_hash(self):
+        fullpath = os.path.abspath(self.name)
+        return hashlib.md5(open(fullpath, 'rb').read()).hexdigest()
+
+    def export(self):
+        call(["path\\to\\aseprite.exe", "--batch", self.name, "--sheet", self.png_name])
+
+    @classmethod
+    def find_file(cls, filename):
+        for file in cls.files:
+            if file.name == filename:
+                return file
+            return false
+
+if __name__ == "__main__":
+    main()
